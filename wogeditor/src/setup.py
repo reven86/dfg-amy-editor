c4b0ac9eb06cb28ea4b1c2de71cad2c3455c6dfd
nitrozark <nitrozark@users.sourceforge.net>
2008-11-10 18:27:08 +0000

    - made wogeditor a console application to help tracking error.

--- wogeditor/src/setup.py
+++ wogeditor/src/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['wogfile.py', 'scanbinfile.py', 'scanxmlfile.py'],
-      windows=[{"script":"wogeditor.py"}],
+#setup(console=['wogfile.py', 'scanbinfile.py', 'scanxmlfile.py'],
+#      windows=[{"script":"wogeditor.py"}],
+setup(console=['wogfile.py', 'scanbinfile.py', 'scanxmlfile.py', "wogeditor.py"],
       options={"py2exe":{"includes":["sip"]}})
